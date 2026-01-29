4da7318fb5f1964ce39e0c71234a01e09b030cb9
mask <maskdotdev@gmail.com>
2026-01-29 06:21:47 -0600

    chore: remove next-docs (replaced by ray-docs)

--- next-docs/app/docs/layout.tsx
+++
@@ -1,6 +0,0 @@
-import type { ReactNode } from "react";
-import DocsShell from "../../components/DocsShell";
-
-export default function DocsLayout({ children }: { children: ReactNode }) {
-  return <DocsShell>{children}</DocsShell>;
-}
